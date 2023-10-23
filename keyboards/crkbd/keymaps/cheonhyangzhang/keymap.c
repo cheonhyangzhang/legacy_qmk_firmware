9f587d0137245f8b6c36c8b06cbbf396e01e3712
Tianxiang Zhang <cenhiangapply@gmail.com>
2023-10-23 09:51:40 -0700

    Update go back and go forwardcurrentBestDate

--- keyboards/crkbd/keymaps/cheonhyangzhang/keymap.c
+++ keyboards/crkbd/keymaps/cheonhyangzhang/keymap.c
@@ -29,10 +29,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // MO(2) provides navigation layer
   // KC_RSHIFT is not often used
   [0] = LAYOUT_split_3x6_3(
-    KC_LGUI,  KC_LALT, KC_W, KC_E,  KC_R,    KC_T,    KC_Y,   KC_U,  KC_I,       KC_O,            KC_NO,    KC_BSPC,
-    KC_Q,     KC_A,    KC_S, KC_D,  KC_F,    KC_G,    KC_H,   KC_J,  KC_K,       KC_L,            KC_ENT,   KC_P,
-    KC_LCTRL, KC_Z,    KC_X, KC_C,  KC_V,    KC_B,    KC_N,   KC_M,  KC_COMM,    KC_DOT,          KC_SLASH, KC_QUOT,
-                             MO(4), KC_LSFT, MO(2),   KC_SPC, MO(3), KC_NO
+    KC_LALT,  KC_LGUI,  KC_W, KC_E,  KC_R,    KC_T,    KC_Y,   KC_U,  KC_I,       KC_O,            KC_NO,    KC_BSPC,
+    KC_Q,     KC_A,     KC_S, KC_D,  KC_F,    KC_G,    KC_H,   KC_J,  KC_K,       KC_L,            KC_ENT,   KC_P,
+    KC_LCTRL, KC_Z,     KC_X, KC_C,  KC_V,    KC_B,    KC_N,   KC_M,  KC_COMM,    KC_DOT,          KC_SLASH, KC_QUOT,
+                              MO(4), KC_LSFT, MO(2),   KC_SPC, MO(3), KC_NO
   ),
  //mouse layer
   [1] = LAYOUT_split_3x6_3(
@@ -88,10 +88,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
   // nums
   [4] = LAYOUT_split_3x6_3(
-      LGUI(LSFT(KC_W)), KC_NO,          KC_NO,          LGUI(LSFT(KC_5)),         KC_NO,              KC_NO,  KC_PGUP,   KC_7, KC_8,   KC_9, KC_NO,           KC_NO,
-      KC_NO,            KC_NO,          LGUI(KC_LBRC),  LCTL(LGUI(LSFT(KC_4))),   LGUI(KC_RBRC),      KC_NO,  KC_PGDN,   KC_4, KC_5,   KC_6, KC_NO,           KC_BSLS,
-      LGUI(KC_Q),       KC_NO,          KC_NO,          LGUI(LSFT(KC_4)),         KC_NO,              KC_NO,  KC_NO,     KC_1, KC_2,   KC_3, LGUI(KC_MINUS),  LGUI(KC_PLUS),
-                                                        KC_NO,                    KC_NO,              KC_NO,  KC_DOT,    KC_0, KC_NO
+      LGUI(LSFT(KC_W)), KC_NO,          KC_NO,          LGUI(LSFT(KC_5)),         KC_NO,          KC_NO,  KC_PGUP,   KC_7, KC_8,   KC_9, KC_NO,   KC_NO,
+      KC_NO,            LGUI(KC_PLUS),  LGUI(KC_LBRC),  LCTL(LGUI(LSFT(KC_4))),   LGUI(KC_RBRC),  KC_NO,  KC_PGDN,   KC_4, KC_5,   KC_6, KC_NO,   KC_BSLS,
+      LGUI(KC_Q),       LGUI(KC_MINUS), KC_NO,          LGUI(LSFT(KC_4)),         KC_NO,          KC_NO,  KC_NO,     KC_1, KC_2,   KC_3, KC_NO,   KC_NO,
+                                                        KC_NO,                    KC_NO,          KC_NO,  KC_DOT,    KC_0, KC_NO
   ),
   //one hand
   //this layer is not up to date
